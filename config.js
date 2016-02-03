9fbd2d996820c543330deb169fc35fc89151f056
victordilella <victordilella@gmail.com>
2016-02-03 10:50:59 -0500

    First Commit. Revamping CRM project from the MEAN Machine book

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://admin:password@ds055515.mongolab.com:55515/crmtestappdb',
+	'secret': 'ilovescotchscotchyscotchscotch'
+};
